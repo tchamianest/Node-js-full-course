011680581b55006339e4a53e915b68339ac424c0
tchami <131756888+tchamianest@users.noreply.github.com>
2023-11-02 12:55:13 +0200

    create New Modules

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -4,6 +4,7 @@
 const fs = require("fs");
 const http = require("http");
 const url = require("url");
+const replaceTemplate = require("./modules/replacetemplate");
 
 // console.log(fs);
 
@@ -65,23 +66,6 @@ const url = require("url");
 
 //// create replace function which change the value of where we call
 
-const replaceTemplate = (temp, product) => {
-  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
-  output = output.replace(/{%IMAGE%}/g, product.image);
-  output = output.replace(/{%FROM%}/g, product.from);
-  output = output.replace(/{%NUTRIENT%}/g, product.nutrients);
-  output = output.replace(/{%QUANTITY%}/g, product.quantity);
-  output = output.replace(/{%PRICE%}/g, product.price);
-  output = output.replace(/{%DESCRIPTION%}/g, product.description);
-  output = output.replace(/{%ID%}/g, product.id);
-
-  if (!product.organic) {
-    output = output.replace(/{%NOT-ORGANIC%}/g, "not-organic");
-  }
-  return output;
-
-  // here we nedd to change the class if the product is not oraganics
-};
 ////////////// create simple server
 const tempover = fs.readFileSync(
   `${__dirname}/templates/template-overview.html`,
---
+++ 1-node-farm/starter/modules/replacetemplate.js
@@ -0,0 +1,17 @@
+module.exports = (temp, product) => {
+  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+  output = output.replace(/{%IMAGE%}/g, product.image);
+  output = output.replace(/{%FROM%}/g, product.from);
+  output = output.replace(/{%NUTRIENT%}/g, product.nutrients);
+  output = output.replace(/{%QUANTITY%}/g, product.quantity);
+  output = output.replace(/{%PRICE%}/g, product.price);
+  output = output.replace(/{%DESCRIPTION%}/g, product.description);
+  output = output.replace(/{%ID%}/g, product.id);
+
+  if (!product.organic) {
+    output = output.replace(/{%NOT-ORGANIC%}/g, "not-organic");
+  }
+  return output;
+
+  // here we nedd to change the class if the product is not oraganics
+};
