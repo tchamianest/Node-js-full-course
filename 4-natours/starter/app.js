236df068340cc3fdec83de4dada4d0e5a07b0eda
tchami <131756888+tchamianest@users.noreply.github.com>
2023-11-15 11:35:14 +0200

    refactoring my code

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -17,18 +17,18 @@ app.use(express.json());
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
-// console.log(tours);
-app.get('/api/v1/tours', (req, res) => {
+
+///refactorie our code
+
+const getallturs = (req, res) => {
   res.status(200).json({
     status: 'success',
     result: tours.length,
     tours,
   });
-});
+};
 
-/// how we can handle the request with the id
-
-app.get('/api/v1/tours/:id', (req, res) => {
+const getonetour = (req, res) => {
   // this can show us the request id that we receive from the serve
   // console.log(req.params);
 
@@ -54,10 +54,9 @@ app.get('/api/v1/tours/:id', (req, res) => {
       tour,
     },
   });
-});
+};
 
-/// how to post something
-app.post('/api/v1/tours', (req, res) => {
+const postnewtour = (req, res) => {
   const newid = tours[tours.length - 1].id + 1;
   const newpost = Object.assign({ id: newid }, req.body);
   tours.push(newpost);
@@ -73,10 +72,9 @@ app.post('/api/v1/tours', (req, res) => {
       });
     }
   );
-});
+};
 
-// how we can update the api info
-app.patch('/api/v1/tours/:id', (req, res) => {
+const updatetours = (req, res) => {
   console.log(tours.length);
   console.log(req.params.id);
   if (req.params.id * 1 > tours.length) {
@@ -91,8 +89,36 @@ app.patch('/api/v1/tours/:id', (req, res) => {
       tours: '<Data Updated>',
     },
   });
-});
+};
 
+const deletetour = (req, res) => {
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'enter the proper ID',
+    });
+  }
+  res.status(204).json({
+    status: 'success',
+  });
+};
+// console.log(tours);
+app.get('/api/v1/tours', getallturs);
+
+/// how we can handle the request with the id
+
+app.get('/api/v1/tours/:id', getonetour);
+
+/// how to post something
+app.post('/api/v1/tours', postnewtour);
+
+// how we can update the api info
+app.patch('/api/v1/tours/:id', updatetours);
+
+/// how to use delete methode for delete the things from database
+app.delete('/api/v1/tours/:id', deletetour);
+
+//listening to the serve
 app.listen(port, () => {
   console.log(`this is the message from server port${port}...`);
 });
