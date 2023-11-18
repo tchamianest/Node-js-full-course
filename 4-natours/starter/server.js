b008abec233686cf60c024fe16e78390fae991a9
tchami <131756888+tchamianest@users.noreply.github.com>
2023-11-18 18:35:32 +0200

    some new package

--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -1,5 +1,9 @@
 const app = require('./app');
+const dotenv = require('dotenv');
 const port = 3000;
+
+dotenv.config({ path: `./Config.env` });
+console.log(process.env);
 
 app.listen(port, () => {
   console.log(`this is the message from server port${port}...`);
